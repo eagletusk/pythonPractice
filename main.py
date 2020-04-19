cc1376b787f1aab3650e43034fafc1c490889558
Paul Subzak <sobczak.paul@gmail.com>
2020-04-19 18:51:57 +0000

    Cleaned up main.py only runs rotateLeft

--- main.py
+++ main.py
@@ -9,19 +9,14 @@ import os
 import random
 import re
 import sys
-import rotateLeft
-import greet
-greet.greet()
 
+# #greet file
+# import greet
+# greet.greet()
+
+#rotateLeft file
+import rotateLeft
 result1 = rotateLeft.go()
 print(result1)
 
 
-
-#     Input arr[] = [1, 2, 3, 4, 5, 6, 7], d = 2, n =7
-# 1) Store d elements in a temp array
-#    temp[] = [1, 2]
-# 2) Shift rest of the arr[]
-#    arr[] = [3, 4, 5, 6, 7, 6, 7]
-# 3) Store back the d elements
-#    arr[] = [3, 4, 5, 6, 7, 1, 2]
