200bac383022b3a599b8de33e5b36e5d46edc908
Paul Subzak <sobczak.paul@gmail.com>
2020-04-19 18:40:53 +0000

    added greet secondary file and it works

---
+++ main.py
@@ -0,0 +1,28 @@
+# python Practice
+
+#rotate left
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import rotateLeft
+import greet
+greet.greet()
+
+# a= [1,2,3]
+# d = 1
+# result = rotLeft(a, d)
+# print(result)
+
+
+#     Input arr[] = [1, 2, 3, 4, 5, 6, 7], d = 2, n =7
+# 1) Store d elements in a temp array
+#    temp[] = [1, 2]
+# 2) Shift rest of the arr[]
+#    arr[] = [3, 4, 5, 6, 7, 6, 7]
+# 3) Store back the d elements
+#    arr[] = [3, 4, 5, 6, 7, 1, 2]
---
+++ rotateLeft.py
@@ -0,0 +1,5 @@
+# Complete the rotLeft function below.
+
+def rotLeft(a, d):
+    # append and pop
+    value = a.pop(d)
