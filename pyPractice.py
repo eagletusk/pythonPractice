f8c0182f7b881252dd706ad9c8dfa2b5869d9372
Paul Subzak <sobczak.paul@gmail.com>
2020-04-21 00:40:21 +0000

    python implementation of binary search

--- pyPractice.py
+++ pyPractice.py
@@ -196,6 +196,25 @@ class Solution:
             if x not in a:
                 return x
 
+# abc = []            
+# abc = Solution.missingNumber(abc,[0,1,2,4])   
+# print(abc)
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+        while l<=r:
+            mid = r+l//2
+            if target == nums[mid]:
+                return mid
+            if target > nums[mid]:
+                # on the right 
+                l = mid+1
+            else:
+                r = mid-1
+        return -1      
+
 abc = []            
-abc = Solution.missingNumber(abc,[0,1,2,4])   
+abc = Solution.search(abc,[0,1,2,4],4)   
 print(abc)
