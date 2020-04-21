3a70798a7eb0804b0490bd82f928e5059d9a047b
Paul Subzak <sobczak.paul@gmail.com>
2020-04-21 17:21:42 +0000

    Find odd count of values in python was able to define function go in multiple places uses x.count which works on a list

--- pyPractice.py
+++ pyPractice.py
@@ -215,6 +215,20 @@ class Solution:
                 r = mid-1
         return -1      
 
-abc = []            
-abc = Solution.search(abc,[0,1,2,4],4)   
-print(abc)
+# abc = []            
+# abc = Solution.search(abc,[0,1,2,4],4)   
+# print(abc)
+
+def go():
+  def find_it(seq):
+      for i in seq:
+          if seq.count(i)%2 != 0:
+              return i
+      return None
+
+  test.assert_equals(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]), 5)
+  test.assert_equals(find_it([1,1,2,-2,5,2,4,4,-1,-2,5]), -1); 
+  test.assert_equals(find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]), 5);
+  test.assert_equals(find_it([10]), 10);
+  test.assert_equals(find_it([1,1,1,1,1,1,10,1,1,1,1]), 10);
+  test.assert_equals(find_it([5,4,3,2,1,5,4,3,2,10,10]), 1);
