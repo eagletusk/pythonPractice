62a8de6cd3513fb2fd3bcc789ac487dd6325ec27
Paul Subzak <sobczak.paul@gmail.com>
2020-04-22 21:49:54 +0000

    added FindNumberswithEvenNumberofDigits.py

---
+++ FindNumberswithEvenNumberofDigits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        bkt =[]
+        for n in nums:
+            count = str(n).count('')-1
+            print (str(n), count)
+            if count%2 == 0 :
+                bkt.append(count)
+                count =0
+            count =0
+        return len(bkt)
+            
+        
