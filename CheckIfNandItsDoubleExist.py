77acd31376d48640fda7d7fe2daa5353de44b720
Paul Subzak <sobczak.paul@gmail.com>
2020-04-24 17:58:52 +0000

    #CheckIfNandItsDoubleExist problem works, got a leetcode problem to work in Relpit it's kinda involved. 0. set up main.py to import the #CheckIfNandItsDoubleExist.py and put from typing import List to get the Leet code arguments to execute 1. make an object of class Solution 2. run object

--- CheckIfNandItsDoubleExist.py
+++ CheckIfNandItsDoubleExist.py
@@ -2,19 +2,17 @@
 from typing import List
 
 class Solution:
-  def checkIfExist(self, arr: List[int]) -> bool:
-      arr = [7,1,14,11]
-      bkt = []
-      for i in arr:
-        '''n = 2*M'''
-        n = i*2
-        print(n in bkt, n, bkt)
-        if n in bkt :        
-          return True
-        else:
-          bkt.append(n)
-      a = [4,2,3,1,5,7]
-      if 7 in a:        
+    def checkIfExist(self, arr: List[int]) -> bool:
+        bkt = set(arr)
+        n=0
+        for i in arr:
+            if i !=0:
+                n = i*2
+            print(n in bkt, n, bkt)
+            if n in bkt :
+                return True
+        return False
+
 
 sol = Solution()
-sol.checkIfExist([21])
+print(sol.checkIfExist([-2,0,10,-19,4,6,-8]))
