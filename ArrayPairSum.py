8daad97164c961a2f74d8bb1d72acac723aa7f29
Paul Subzak <sobczak.paul@gmail.com>
2020-04-24 23:35:09 +0000

    added to ArrayPairSum.py alternative solution

--- ArrayPairSum.py
+++ ArrayPairSum.py
@@ -1,23 +1,41 @@
 def pair_sum(arr,k):
     
-    count = 0    
-    # arr.sort()
-    lst = arr
-    # print (lst)
-    i = 0
+    # count = 0    
+    # # arr.sort()
+    # lst = arr
+    # # print (lst)
+    # i = 0
 
-    while i < len(lst):
-      # print("k-1 =", k-lst[i], lst)
-      if k-lst[i] in lst:
-        # print("k-1 =", k-lst[i], lst)
-        count +=1
-        lst.remove(k-lst[i])
-        lst.remove(lst[i])
-        # print(lst, i, k-i, count)
-        i = 0
-      else:
-        i+= 1
-    return count
+    # while i < len(lst):
+    #   # print("k-1 =", k-lst[i], lst)
+    #   if k-lst[i] in lst:
+    #     # print("k-1 =", k-lst[i], lst)
+    #     count +=1
+    #     lst.remove(k-lst[i])
+    #     lst.remove(lst[i])
+    #     # print(lst, i, k-i, count)
+    #     i = 0
+    #   else:
+    #     i+= 1
+    # return count
+
+    #  alternate solution:
+  if len(arr)<2:
+    return
+
+  seen = set()
+  output = set()
+
+  for num in arr:
+    target = k-num
+    print(seen, output)
+    if target not in seen:
+      seen.add(num)
+
+    else:
+      output.add((min(num,target), max(num,target)))
+
+  return len(output)
 
 
 """
