9c5843947a61635e3b1a470373df3f72b7caea84
Paul Subzak <sobczak.paul@gmail.com>
2020-04-25 18:25:54 +0000

    added stringCompression.py solution added

--- StringCompression.py
+++ StringCompression.py
@@ -2,11 +2,31 @@
 
 '''AAAABBBBCCCCCDDEEEE to A4B4C5D2E4'''
 def compress(s):
-  c = {}
+  r = ""
+  lent = len(s)
 
-  for i in s:
-    c.add(i)
+  if lent ==0:
+    return ''
+
+  if lent ==1:
+    return s + '1'
   
+  cnt = 1
+  i = 1
+
+  while i< lent:
+
+    if s[i] == s[i-1]:
+      cnt +=1
+    else:
+      r = r + s[i-1] + str(cnt)
+      cnt =1
+    i+=1
+  
+  r = r + s[i-1] + str(cnt)
+  print(r)
+  return r
+
 
 
 
